ace383cfa72c3ec6632b80e1a69dea0457f663b2
DerekJLeggett <DerekJLeggett@gmail.com>
2023-02-02 07:10:44 -0700

    update

--- src/main/java/com/leggett/runner/RunnerApplication.java
+++ src/main/java/com/leggett/runner/RunnerApplication.java
@@ -2,6 +2,8 @@ package com.leggett.runner;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +16,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import jp.vmi.selenium.selenese.Parser;
 import jp.vmi.selenium.selenese.Runner;
+import jp.vmi.selenium.selenese.Selenese;
+import jp.vmi.selenium.selenese.TestSuite;
+import jp.vmi.selenium.selenese.result.Result;
+import static jp.vmi.selenium.selenese.result.Unexecuted.*;
 
 @SpringBootApplication
 public class RunnerApplication implements CommandLineRunner {
@@ -42,16 +49,29 @@ public class RunnerApplication implements CommandLineRunner {
         chromeOptions.setCapability("se:screenResolution", "1920x1080");
         chromeOptions.setCapability("se:timeZone", "US/Arizona");
         WebDriver driver;
+        Runner runner = new Runner();
         try {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
-            //driver = new ChromeDriver();
+            // driver = new ChromeDriver();
             driver.manage().window().maximize();
-            
-            Runner runner = new Runner();
             runner.setDriver(driver);
             runner.setSpeed(100);
             runner.setHtmlResultDir("target\\test-output");
-            runner.run("src\\main\\resources\\Leggett.side");
+            // runner.run("src\\main\\resources\\Leggett.side");
+            List<TestSuite> testSuiteList = new ArrayList<>();
+            Selenese selenese = Parser.parse("src\\main\\resources\\Leggett.side", runner.getCommandFactory());
+            testSuiteList.add((TestSuite) selenese);
+            Result totalResult = UNEXECUTED;
+            Result result = null;
+            
+            for (TestSuite testSuite : testSuiteList) {
+                try {
+                    result = runner.execute(testSuite);
+                } catch (Exception e) {
+                    LOG.error(e.getMessage());
+                }
+                totalResult = totalResult.updateWithChildResult(testSuite, result);
+            }
             driver.close();
         } catch (Exception e) {
             LOG.error(e.getMessage());
